51aef9431614e947e5c04f6af61d0593cbd38e91
Matthew Faltys <mfaltys@gmail.com>
2016-06-22 00:07:23 -0500

    Add client test script

---
+++ clientrun.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# this is designed to be a generic runscript that is the be the command
+# ran from all docker clients.  this script will take care of the
+# registration and then launch whatever app the container is for.
+
+echo "$1 nsproxy_master" >> /etc/hosts
+SERVER_IP=nsproxy_master
+SERVER_PORT=8080
+APP_NAME=testapp1
+CLUSTER_NAME=smartos
+
+curl -d hostname=$APP_NAME -d cluster=$CLUSTER_NAME $SERVER_IP:$SERVER_PORT
+
+echo "app is now running..."
+
+while :
+do
+	sleep 100
+done
