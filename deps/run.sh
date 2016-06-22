bca9a0167265f26437394fe1bfe857e2d6be57ed
Matthew Faltys <mfaltys@mgail.com>
2016-06-22 13:19:10 -0500

    Add formatting

--- deps/run.sh
+++ deps/run.sh
@@ -7,4 +7,11 @@ echo "save 30 1" >> /redis.conf
 
 redis-server /redis.conf
 
+echo -e "\e[36m ___ ___ ___ ___ ___ _ _ _ _ \e[39m"
+echo -e "\e[36m|   |_ -| . |  _| . |_'_| | |\e[39m"
+echo -e "\e[36m|_|_|___|  _|_| |___|_._|_  |\e[39m"
+echo -e "\e[36m        |_|             |___|\e[39m"
+echo -e "         \e[3m:: \e[31mv0.0.2b PRE-RELEASE  \e[39m::\e[0m  "
+
+
 /nsproxy $@
