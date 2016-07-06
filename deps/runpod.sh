4a44714c54a5e43ae2a63f443f3645721de95696
Matthew Faltys <mfaltys@gmail.com>
2016-07-06 11:05:05 -0500

    Fix weighted tests

--- deps/runpod.sh
+++ deps/runpod.sh
@@ -4,22 +4,25 @@ sudo docker run \
 	-d \
 	-p 4410:4410 \
 	-e APP_PORT=4410 \
+	-e WEIGHT=3 \
 	-e CLUSTER_NAME=testapp \
 	--name=testapp0 \
-	mfaltys/pod:4 demoapp $1
+	mfaltys/pod:6 demoapp $1
 
 sudo docker run \
 	-d \
 	-p 4411:4411 \
 	-e APP_PORT=4411 \
+	-e WEIGHT=1 \
 	-e CLUSTER_NAME=testapp \
 	--name=testapp1 \
-	mfaltys/pod:4 demoapp $1
+	mfaltys/pod:6 demoapp $1
 
 sudo docker run \
 	-d \
 	-p 4412:4412 \
 	-e APP_PORT=4412 \
+	-e WEIGHT=1 \
 	-e CLUSTER_NAME=testapp \
 	--name=testapp2 \
-	mfaltys/pod:4 demoapp $1
+	mfaltys/pod:6 demoapp $1
