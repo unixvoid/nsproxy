0506bf6ca8662961af213f0d13cdd6e9ff60d8d1
Matthew Faltys <mfaltys@gmail.com>
2016-07-10 21:54:42 -0500

    Add spelling error

--- nsproxy_tests/test_dns_spec.go
+++ nsproxy_tests/test_dns_spec.go
@@ -11,26 +11,26 @@ func testDnsSpec(hostUrl *string) {
 	// correct
 	body, returnVal := twoKeyPostReturnEndpoint(*hostUrl, "/dnsspec", "domain", "test.domain", "dnstype", "a")
 	checkResponse("Correct", 200, returnVal)
-	glogger.Info.Printf("/dnsspec GET reponds: %s", body)
+	glogger.Info.Printf("/dnsspec POST reponds: %s", body)
 	glogger.Info.Println("")
 
 	glogger.Info.Println("Testing :: POST to /dnsspec AAAA type")
 	// correct
 	body, returnVal = twoKeyPostReturnEndpoint(*hostUrl, "/dnsspec", "domain", "test.domain", "dnstype", "aaaa")
 	checkResponse("Correct", 200, returnVal)
-	glogger.Info.Printf("/dnsspec GET reponds: %s", body)
+	glogger.Info.Printf("/dnsspec POST reponds: %s", body)
 	glogger.Info.Println("")
 
 	glogger.Info.Println("Testing :: POST to /dnsspec CNAME type")
 	// correct
 	body, returnVal = twoKeyPostReturnEndpoint(*hostUrl, "/dnsspec", "domain", "test.domain", "dnstype", "cname")
 	checkResponse("Correct", 200, returnVal)
-	glogger.Info.Printf("/dnsspec GET reponds: %s", body)
+	glogger.Info.Printf("/dnsspec POST reponds: %s", body)
 	glogger.Info.Println("")
 
 	glogger.Info.Println("Testing :: POST to /dnsspec no type")
 	// correct
 	body, returnVal = twoKeyPostReturnEndpoint(*hostUrl, "/dnsspec", "domain", "test.domain", "dnstype", "a")
 	checkResponse("Correct", 200, returnVal)
-	glogger.Info.Printf("/dnsspec GET reponds: %s", body)
+	glogger.Info.Printf("/dnsspec POST reponds: %s", body)
 }
